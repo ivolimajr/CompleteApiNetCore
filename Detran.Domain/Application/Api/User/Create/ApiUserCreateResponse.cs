e5a38343418c111432a134af233496f8e3982402
Ivo Lima Jr <ivo.sl.jr@gmail.com>
2021-11-14 17:46:46 -0300

    git ignore

--- Detran.Domain/Application/Api/User/Create/ApiUserCreateResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Detran.Domain.Application.Api.User.Create
-{
-    public class ApiUserCreateResponse
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Name { get; set; }
-    }
-}
